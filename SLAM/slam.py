8870dff2dd0c5212c50a717277fedd4a77efab2d
ars1208 <ara.kaz1208@gmail.com>
2024-11-26 22:36:29 +0900

    feat: show by pygame

--- SLAM/slam.py
+++ SLAM/slam.py
@@ -1,11 +1,20 @@
 import cv2
+import pygame
 
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+W = 3840 // 2
+H = 2160 // 2
+
+pygame.init()
+screen = pygame.display.set_mode((W,H))
+# surface = pygame.Surface((W,H)).convert()
+# cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 
 def process_frame(img):
-    img = cv2.resize(img, (3840//2, 2160//2))
-    cv2.imshow('image', img)
-    cv2.waitKey()
+    img = cv2.resize(img, (W, H))
+    surf = pygame.surfarray.make_surface(img)
+    
+    screen.blit(surf, (0,0))
+    pygame.display.flip()
     print(img.shape)
 
 if __name__ == "__main__":
